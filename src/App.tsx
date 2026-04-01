64e4f824174313cbe3f54de2f52353bda2560566
Yifei Li <yifeili@Yifeis-MacBook-Pro.local>
2026-04-01 17:22:47 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import AgoraVoiceCalling from "./components/voiceCalling";
+import { useState } from "react";
+function App() {
+  
+  
+  
+  return (
+    <>
+     < AgoraVoiceCalling/>
+    </>
+  )
+}
+
+export default App
